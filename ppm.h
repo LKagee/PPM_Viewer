7be065d2dbf9d0e75665f198c3ae5e9ada961c29
KKage <jaataa090@gmail.com>
2026-05-19 10:45:56 -0400

    Added magic number reader (P16 only), width and height reader, and max color reader. Begun drafting body reader
    Signed-off-by: KKage <jaataa090@gmail.com>

--- ppm.h
+++ ppm.h
@@ -1,7 +1,10 @@
 #pragma once
-#define ERROR(a) printf("ERROR IN PPM READER!: %s", a);
+#define ERROR(a) fprintf(stdout, "ERROR IN PPM READER!: %s", a)
 #include <stdint.h>
 
+typedef struct {
+	int r, g, b;
+} vec3;
 
 typedef struct {
 
@@ -10,15 +13,15 @@ typedef struct {
 	int image_width;
 	int image_height;
 	int color_max;
-	int* vertices;
+	vec3* vertices;
 
 } ppm;
 
-// Careful of edianness, P16 = 0x00503136 in 32bit hexadecimal but is stored in reverse order 
-// in memory when in char array;
+	// Careful of edianness, P16 = 0x00503136 in 32bit hexadecimal but is stored in reverse order 
+	// in memory when in char array;
 typedef enum {
  
-	P16 = 0x00363150
+	P3 = 0x00003350
 } HEADER;
 
 typedef struct {
@@ -26,17 +29,52 @@ typedef struct {
 	HEADER headers;
 } tokens;
 
-static __attribute__((always_inline)) void read_file(ppm* PPM, tokens* tokenizer) {
+static /*__attribute__((always_inline))*/ void* read_file(ppm* PPM, tokens* tokenizer) {
+	// Not sure if inlining is slowing render speeds or if its something else, 
+	// So far, the speeds seem to be slightly faster.
 	
 	if(PPM->file_ppm == NULL) printf("error");
+	PPM->vertices = (vec3*)malloc(sizeof(vec3) * 3);
 
 	fscanf(PPM->file_ppm, "%s", PPM->header);
 	int header;
 	memcpy(&header, PPM->header, sizeof(int));
 	switch(header) {
-		case P16: printf("Token: P16\n"); break;
-		default: break;
+		case P3: printf("Token: P3\n"); break;
+		default: ERROR("Invalid file magic number (only P3 is supported)\n"); return NULL; break;
 	}
+
+	// WARNING: Lots of if statements...
+
+	if(fscanf(PPM->file_ppm, "%d %d", &PPM->image_width, &PPM->image_height) != 2) { 
+		ERROR("Invalid file width and height (ensure .ppm file is ordered [width] [height] immediately under magic number)\n"); 
+		return NULL;
+	}
+
+	printf("Width: %d. Height: %d.\n", PPM->image_width, PPM->image_height);
+
+	if(!fscanf(PPM->file_ppm, "%d", &PPM->color_max)) {
+		ERROR("Invalid maximum color value (must be 0-255 and immediately under width and height)\n");
+		return NULL;
+	}
+
+	
+	if(PPM->color_max > 255 || PPM->color_max < 0) {
+		ERROR("Invalid maximum color value (must be 0-255 and immediately under width and height)\n");
+		return NULL;
+
+	}
+
+	printf("Color Max: %d.\n", PPM->color_max);
+
+	if(fscanf(PPM->file_ppm, "%d %d %d", &PPM->vertices[0].r, &PPM->vertices[0].g, &PPM->vertices[0].b) != 3) {
+		ERROR("Invalid row 1 RGB data (must be 0-color_max for each RGB cell and be formatted [R] [G] [B])\n");
+	 	return NULL;
+	}
+        printf("RGB: %d %d %d", PPM->vertices[0].r, PPM->vertices[0].g, PPM->vertices[0].b);	
+
+	return PPM;
+
 
 //	printf("%d", header);
 }
