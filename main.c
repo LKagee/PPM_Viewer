455576b9ba22632e299cacf332d5a8b764637a19
KKage <jaataa090@gmail.com>
2026-05-20 14:40:52 -0400

    DEBUG SAVE STATE
    Signed-off-by: KKage <jaataa090@gmail.com>

--- main.c
+++ main.c
@@ -2,30 +2,15 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include "ppm.h"
+#include "main.h"
 
-
-typedef struct {
-	SDL_Window *window;
-	SDL_Surface *surface;
-
-} sdl;
 
 /* Modified to remove gcc warnin g "cast from pointer to integer of different type" because doing uint32_t pixel
  *  = (uint32_t)... does not guarentee the value of the 'pixels' address might be too large on some systems and
  *  doing arithmatic might give unexpected results 
  */
+ 
 
-void put_pixel(sdl* SDL, int x, int y) {
-	
-	if(SDL_MUSTLOCK(SDL->surface))SDL_LockSurface(SDL->surface);
-	uint32_t* pixels = (uint32_t*)SDL->surface->pixels;
-
-
-	uint32_t* pixel = (uint32_t*)((uint8_t*)pixels + y*SDL->surface->pitch + x*sizeof(uint32_t)); 
-	*pixel = SDL_MapRGBA(SDL->surface->format, 0xff, 0xff, 0xff, 0xff);
-	if(SDL_MUSTLOCK(SDL->surface))SDL_UnlockSurface(SDL->surface);
-
-}
 
 int main( void )
 {
@@ -36,9 +21,9 @@ int main( void )
 	read_file(PPM, TOKEN);
 	SDL_Init(SDL_INIT_EVERYTHING);
 	SDL.window = SDL_CreateWindow("render", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 400, SDL_WINDOW_RESIZABLE);
-	SDL_Surface* main = SDL_GetWindowSurface(SDL.window);
+//	SDL_Surface* main = SDL_GetWindowSurface(SDL.window);
 	if(SDL.window == NULL) return 1;
-
+/*
 	SDL.surface = SDL_CreateRGBSurfaceWithFormat(0, 640, 400, 32, SDL_PIXELFORMAT_RGBA8888);
 
 
@@ -57,8 +42,8 @@ int main( void )
 			// ###
 	
 
-
-
+*/
+	render_ppm(&SDL, PPM, TOKEN);
 	SDL_Delay(1000);
 	SDL_Quit();
 	SDL_DestroyWindow(SDL.window);
--- ppm.h
+++ ppm.h
@@ -1,33 +1,9 @@
 #pragma once
 #define ERROR(a) fprintf(stdout, "ERROR IN PPM READER!: %s", a)
+#define SDL_ERROR(a) fprintf(stdout, "ERROR IN SDL!: %s", a)
 #include <stdint.h>
+#include "main.h"
 
-typedef struct {
-	int r, g, b;
-} vec3;
-
-typedef struct {
-
-	FILE* file_ppm;
-	char header[2];
-	int image_width;
-	int image_height;
-	int color_max;
-	vec3* vertices;
-
-} ppm;
-
-	// Careful of edianness, P16 = 0x00503136 in 32bit hexadecimal but is stored in reverse order 
-	// in memory when in char array;
-typedef enum {
- 
-	P3 = 0x00003350
-} HEADER;
-
-typedef struct {
-
-	HEADER headers;
-} tokens;
 
 static /*__attribute__((always_inline))*/ void* read_file(ppm* PPM, tokens* tokenizer) {
 	// Not sure if inlining is slowing render speeds or if its something else, 
@@ -67,14 +43,60 @@ static /*__attribute__((always_inline))*/ void* read_file(ppm* PPM, tokens* toke
 
 	printf("Color Max: %d.\n", PPM->color_max);
 
-	if(fscanf(PPM->file_ppm, "%d %d %d", &PPM->vertices[0].r, &PPM->vertices[0].g, &PPM->vertices[0].b) != 3) {
+	for(int i = 0; i < PPM->image_height * PPM->image_width; i++) {
+
+	if(fscanf(PPM->file_ppm, "%d %d %d", &PPM->vertices[i].r, &PPM->vertices[i].g, &PPM->vertices[i].b) != 3) {
 		ERROR("Invalid row 1 RGB data (must be 0-color_max for each RGB cell and be formatted [R] [G] [B])\n");
 	 	return NULL;
 	}
-        printf("RGB: %d %d %d", PPM->vertices[0].r, PPM->vertices[0].g, PPM->vertices[0].b);	
+        printf("RGB: %d %d %d\n", PPM->vertices[i].r, PPM->vertices[i].g, PPM->vertices[i].b);
+	}	
 
 	return PPM;
 
 
 //	printf("%d", header);
 }
+
+static void* render_ppm(sdl* SDL, ppm* PPM, tokens *tokenizer) {
+
+	SDL_Surface* main = SDL_GetWindowSurface(SDL->window);
+	SDL->surface = SDL_CreateRGBSurfaceWithFormat(0, 640, 400, 32, SDL_PIXELFORMAT_RGBA8888);
+
+
+	for(int i = 0; i < 400; i++) {
+	   for(int k = 0; k < 640; k++) {   
+	
+	   put_pixel(SDL, k, i);
+	   }
+	}
+	   SDL_BlitSurface(SDL->surface, 0, main, 0); // For some reason just using SDL.surface is not good enough for SDL_UpdateWindowSurface? What the shit?
+	   int ret=SDL_UpdateWindowSurface(SDL->window);
+	   if(ret<0) {
+		SDL_ERROR(SDL_GetError());
+		return NULL;
+	   }
+
+	   return SDL;
+
+
+}
+
+/* int render(int x, int y, ppm* PPM, sdl* SDL, int color_ptr) {
+ * if(x >= (((width/image_width) *3)-1)) {
+ * 	x = 0;
+ * 	y++;
+ * }
+ * if(y >= height) return 0;
+ *	for(int i=x, i < x + width/image_width; i++) {
+ *		put_pixel(SDL, i, y, PPM->vertices[color_ptr].r, PPM->vertices[color_ptr].g, PPM->vertices[color_ptr].b);
+ *	}
+ *	
+ *	return render(x+(width/image_width), y, PPM, SDL, color_ptr++);
+ *
+ * }
+ *
+ *
+ *
+ *
+ * */
